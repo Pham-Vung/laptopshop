4a69dfb17dc5eca65a512c3fab688c7a5d1ae218
Pham Van Vung <phamvanvung211003@gmail.com>
2025-05-23 22:59:35 +0700

    create user frontend

--- src/main/java/org/example/laptopshop/controller/UserController.java
+++ src/main/java/org/example/laptopshop/controller/UserController.java
@@ -3,6 +3,7 @@ package org.example.laptopshop.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.laptopshop.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +12,14 @@ public class UserController {
     private final UserService userService;
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        String test = userService.handleHello();
+        model.addAttribute("vincent", test);
         return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getAdminUserPage(Model model) {
+        return "admin/user/create";
     }
 }
---
+++ src/main/java/org/example/laptopshop/entity/User.java
@@ -0,0 +1,16 @@
+package org.example.laptopshop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+    private long id;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phone;
+}
