a6337fae544e799a58a577006129157545a8a8da
Pham Van Vung <phamvanvung211003@gmail.com>
2025-05-28 21:21:40 +0700

    submit form

--- src/main/java/org/example/laptopshop/controller/UserController.java
+++ src/main/java/org/example/laptopshop/controller/UserController.java
@@ -1,10 +1,14 @@
 package org.example.laptopshop.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.example.laptopshop.entity.User;
 import org.example.laptopshop.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,7 +23,14 @@ public class UserController {
     }
 
     @RequestMapping("/admin/user")
-    public String getAdminUserPage(Model model) {
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("Run here: " + user);
+        return "hello";
     }
 }
