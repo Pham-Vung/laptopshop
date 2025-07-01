002d11bd606133598332df333e246d0aa4501b7c
Pham Van Vung <phamvanvung211003@gmail.com>
2025-07-01 15:48:57 +0700

    complete client layout

---
+++ src/main/java/org/example/laptopshop/controller/client/ItemController.java
@@ -0,0 +1,17 @@
+package org.example.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ItemController {
+
+    @GetMapping("/{id}")
+    public String getProductPage(@PathVariable long id) {
+        return "client/product/detail";
+    }
+
+}
